4ec1b6384b21e51a17bf37a8b4dd579dd53002a0
tainadominguess <132912429+tainadominguess@users.noreply.github.com>
2023-05-10 10:45:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-echo "Olá,turma do az900";
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Minha pagina inicial</title>
+  </head>
+  <body>
+    <h1>Minha pagina inicial</h1>
+    <p>Bem vindo a minha pagina inicial!Aqui você encontrara informaçôes sobre mim e meus interesses.</p>
+    <p>Sou uma aluna iniciante em programaçâo e cloud computing!
+      gosto tambem de jogar volei,fazer exercicios fisicos,treinar e viajar.<p>
+    <a href="#" class ="button">Entre em contato</a>
+  </body>
+</html>
