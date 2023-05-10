989f6ed1193588a6ec1249cb83ebd1ac84632861
tainadominguess <132912429+tainadominguess@users.noreply.github.com>
2023-05-10 11:28:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <h1>Minha pagina inicial</h1>
     <p>Bem vindo a minha pagina inicial!Aqui você encontrara informaçôes sobre mim e meus interesses.</p>
     <p>Sou uma aluna iniciante em programaçâo e cloud computing!
-      gosto tambem de jogar volei,fazer exercicios fisicos,treinar e viajar.<p>
+      gosto tambem de jogar volei,fazer exercicios fisicos,treinar e viajar.</p>
     <a href="#" class ="button">Entre em contato</a>
   </body>
 </html>
